160a29eb88980ddb8cf78cf83f977b06d2434d88
Ghozi <mghozighifar04@gmail.com>
2026-07-23 19:09:17 +0700

    menambahkan fungsi navbar pada main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#0F172A] text-white flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">Muhammad Ghozi Al Ghifari</h1>
+    <>
+      <Navbar />
 
-        <p className="mt-4 text-xl text-slate-400">
-          Server Infrastructure Engineer
-        </p>
-        <p className="mt-4 text-xl text-slate-400">
-          Database Administrator
-        </p>
-        <p className="mt-4 text-xl text-slate-400">
-          Front End Developer
-        </p>
+      <main className="min-h-screen bg-[#0F172A] text-white flex items-center justify-center">
+        <div className="text-center">
 
-        <p className="mt-2 text-slate-500">
-          Building reliable infrastructure and modern web applications.
-        </p>
-      </div>
-    </main>
+          <h1 className="text-5xl font-bold">
+            Muhammad Ghozi Al Ghifari
+          </h1>
+
+          <p className="mt-4 text-xl text-slate-400">
+            Server Infrastructure Engineer
+          </p>
+
+          <p className="mt-2 text-slate-400">
+            Database Administrator
+          </p>
+
+          <p className="mt-2 text-slate-400">
+            Front End Developer
+          </p>
+
+          <p className="mt-6 text-slate-500 max-w-xl mx-auto">
+            Building reliable infrastructure and modern web applications.
+          </p>
+
+        </div>
+      </main>
+    </>
   );
 }
