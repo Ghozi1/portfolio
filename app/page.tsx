d853658ed6db4923ebd007fb2d64eaa6c6cbf8ea
Ghozi <mghozighifar04@gmail.com>
2026-07-23 19:07:18 +0700

    phase 1

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#0F172A] text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Muhammad Ghozi Al Ghifari</h1>
+
+        <p className="mt-4 text-xl text-slate-400">
+          Server Infrastructure Engineer
+        </p>
+        <p className="mt-4 text-xl text-slate-400">
+          Database Administrator
+        </p>
+        <p className="mt-4 text-xl text-slate-400">
+          Front End Developer
+        </p>
+
+        <p className="mt-2 text-slate-500">
+          Building reliable infrastructure and modern web applications.
+        </p>
+      </div>
+    </main>
+  );
+}
